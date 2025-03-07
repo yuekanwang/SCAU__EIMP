7e2d4f45cf74dd4323e0f6c48ae3a30751c63caf
shanmu <1994225702@qq.com>
2025-03-07 15:55:56 +0800

    init project structure

---
+++ EIMP/src/main/java/com/eimp/App.java
@@ -0,0 +1,19 @@
+package com.eimp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
