2e04d8bc79b2e51ff59f4d2733793d4cf9bb26b6
yuekanwang <3081906907@qq.com>
2025-03-08 14:00:00 +0800

    fxml不能导入

--- EIMP/src/main/java/com/eimp/App.java
+++ EIMP/src/main/java/com/eimp/App.java
@@ -3,14 +3,20 @@ package com.eimp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320,  240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
---
+++ EIMP/src/main/java/com/eimp/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.eimp.Controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HomeController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
