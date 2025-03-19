d32b22adce3191f52c301b17707dd9dc4bf1d0ec
shanmu <1994225702@qq.com>
2025-03-19 20:34:26 +0800

    修改幻灯片UI,大部分已修改,缩略图UI待确定

--- EIMP/src/main/java/com/eimp/App.java
+++ EIMP/src/main/java/com/eimp/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Slide_test.class.getResource("/fxml/WindowMain.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SlideWindow.class.getResource("/fxml/WindowMain.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 700);
         stage.setTitle("电子图片管理系统");
         stage.setScene(scene);
