1a5c9f9259e72385ae8535c71904dcb0b1199fe6
yuekanwang <3081906907@qq.com>
2025-03-22 15:33:38 +0800

    对主界面进行了美化

--- EIMP/src/main/java/com/eimp/App.java
+++ EIMP/src/main/java/com/eimp/App.java
@@ -3,6 +3,7 @@ package com.eimp;
 import com.eimp.Controller.ControllerMap;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
