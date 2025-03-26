2b18f3d151ece4e54e5dfa995c44f9991b2cd27e
yuekanwang <3081906907@qq.com>
2025-03-26 10:44:51 +0800

    解决了最大化过大的问题

--- EIMP/src/main/java/com/eimp/App.java
+++ EIMP/src/main/java/com/eimp/App.java
@@ -5,9 +5,11 @@ import com.eimp.controller.WindowMainController;
 import com.eimp.controller.WindowSlideController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import java.io.IOException;
@@ -28,10 +30,33 @@ public class App extends Application {
 
         Image Appicon = new Image(getClass().getResourceAsStream("/icon2/EIMP.png"));
         stage.getIcons().add(Appicon);
-//        stage.initStyle(StageStyle.UNDECORATED);
-
+        configureSmartMaximize(stage);//配置智能最大化
         stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
+    }
+
+    private void configureSmartMaximize(Stage stage) {
+        // 获取屏幕可视区域（扣除任务栏），用Screen得到屏幕可视化区域，去除掉操作系
+        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+
+        // 窗口最大化监听器,这里是把窗口最大化的范围改了
+
+/*      stage.maximizedProperty()：获取窗口的“是否最大化”属性（BooleanProperty 类型）。
+        addListener：添加监听器，当最大化状态变化时触发回调。
+        newVal：表示窗口是否进入最大化状态（true 为最大化，false 为取消最大化）。*/
+        stage.maximizedProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal) {
+                // 应用修正后的最大化尺寸
+                stage.setX(visualBounds.getMinX());
+                stage.setY(visualBounds.getMinY());
+                stage.setWidth(visualBounds.getWidth());
+                stage.setHeight(visualBounds.getHeight());
+            }
+        });
+
+        // 初始化时设置最大化参数
+        stage.setMaxWidth(visualBounds.getWidth());
+        stage.setMaxHeight(visualBounds.getHeight());
     }
 
     public static Stage getStage() {
